3b6116ae30323ca16ae8e16e312207da64b66d7e
arnaud <lebon.arnaud00@gmail.com>
2020-02-17 14:39:09 +0100

    test#1 --Classes created with methods

---
+++ src/main/java/me/guillaume/tournament/Swordsman.java
@@ -0,0 +1,9 @@
+package me.guillaume.tournament;
+
+public class Swordsman {
+    public void engage(Viking viking) {
+    }
+
+    public boolean hitPoints() {
+    }
+}
---
+++ src/main/java/me/guillaume/tournament/Viking.java
@@ -0,0 +1,7 @@
+package me.guillaume.tournament;
+
+public class Viking {
+    public boolean hitPoints() {
+        return false;
+    }
+}
