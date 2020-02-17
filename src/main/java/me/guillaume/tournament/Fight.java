db568cf53214531933276aba53f41a2181e23287
Alexandre <alascenci@gmail.com>
2020-02-17 15:55:24 +0100

    Intégration de la notion de combat.

---
+++ src/main/java/me/guillaume/tournament/Fight.java
@@ -0,0 +1,16 @@
+package me.guillaume.tournament;
+
+import java.util.ArrayList;
+
+public class Fight {
+    private ArrayList<Fighter> fighters;
+
+    public Fight(ArrayList<Fighter> fighters) {
+        this.fighters = fighters;
+        startFight();
+    }
+
+    private void startFight() {
+
+    }
+}
