9918e5aba77e1dec063b6aef86d1727c35c5f1c7
Alexandre <alascenci@gmail.com>
2020-02-17 17:56:14 +0100

    Combat entre un épéiste et un viking.

--- src/main/java/me/guillaume/tournament/Fight.java
+++ src/main/java/me/guillaume/tournament/Fight.java
@@ -6,6 +6,15 @@ public class Fight {
     }
 
     private void startFight(Fighter f1, Fighter f2) {
+        int turn = 0;
 
+        while (f1.hitPoints() > 0 && f2.hitPoints() > 0) {
+            if (turn == 0) {
+                f1.hit(f2);
+            } else {
+                f2.hit(f1);
+            }
+            turn = (turn + 1) % 2;
+        }
     }
 }
--- src/main/java/me/guillaume/tournament/Fighter.java
+++ src/main/java/me/guillaume/tournament/Fighter.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public abstract class Fighter {
     protected int hp;
     protected int dmg;
-    protected ArrayList<Portable> arms;
+    protected ArrayList<Portable> arms = new ArrayList<>();
 
     public Fighter() {
     }
@@ -13,12 +13,16 @@ public abstract class Fighter {
     public Fighter(String state) {
     }
 
-    protected void setHp(int hp) {
-        this.hp = hp;
+    public int getDmg() {
+        return dmg;
     }
 
     protected void setDmg(int dmg) {
         this.dmg = dmg;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
     }
 
     public int hitPoints() {
@@ -31,5 +35,13 @@ public abstract class Fighter {
 
     public void engage(Fighter other) {
         new Fight(this, other);
+    }
+
+    public void hit(Fighter other) {
+        other.getHitBy(this);
+    }
+
+    protected void getHitBy(Fighter other) {
+        setHp(Math.max(0, hitPoints() - other.getDmg()));
     }
 }
