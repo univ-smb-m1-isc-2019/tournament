b83cff82292d088a092800b1d54fc4d07b18b78c
Baptiste <baptiste.diot@hotmail.fr>
2020-02-17 16:28:51 +0100

    Added engage method for a fighter to attack another one to the death, First test passed

--- src/main/java/me/guillaume/tournament/Fighter.java
+++ src/main/java/me/guillaume/tournament/Fighter.java
@@ -1,18 +1,48 @@
 package me.guillaume.tournament;
 
-public class Fighter {
+public abstract class Fighter {
 
     private int hitPoints;
+    private Weapon weapon;
 
-    public Fighter(int hitPoints){
+    public Fighter(int hitPoints, Weapon weapon){
         this.hitPoints = hitPoints;
+        this.weapon = weapon;
     }
 
     public void engage(Fighter otherFighter){
+        while(!isDead()){
+            this.attack(otherFighter);
 
+            if(otherFighter.isDead()) break;
+
+            otherFighter.attack(this);
+        }
+    }
+
+    private boolean isDead(){
+        return hitPoints <= 0;
+    }
+
+    private void attack(Fighter otherFighter){
+        int attackerAttackPower = this.getAttackPower();
+
+        otherFighter.takeAHit(attackerAttackPower);
+    }
+
+    private int getAttackPower(){
+        return weapon.getPower();
+    }
+
+    private void takeAHit(int damageTaken){
+        hitPoints -= damageTaken;
+
+        if(hitPoints < 0) hitPoints = 0;
     }
 
     public int hitPoints(){
         return hitPoints;
     }
+
+    public abstract Fighter equip(String equipment);
 }
