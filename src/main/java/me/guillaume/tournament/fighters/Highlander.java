a5595b511020336b1a3f1cbb2f3f16c00d8af99e
Alexandre <alascenci@gmail.com>
2020-02-18 17:50:48 +0100

    Mise à jour Highlander et intégration GreatSword.

--- src/main/java/me/guillaume/tournament/fighters/Highlander.java
+++ src/main/java/me/guillaume/tournament/fighters/Highlander.java
@@ -1,7 +1,11 @@
 package me.guillaume.tournament.fighters;
 
+import me.guillaume.tournament.fighters.weapons.GreatSword;
+
 public class Highlander extends Fighter {
     public Highlander() {
+        setHp(150);
+        setWeapon(new GreatSword());
     }
 
     public Highlander(String state) {
---
+++ src/main/java/me/guillaume/tournament/fighters/weapons/GreatSword.java
@@ -0,0 +1,9 @@
+package me.guillaume.tournament.fighters.weapons;
+
+public class GreatSword extends TwoHandWeapon {
+    public GreatSword() {
+        super();
+
+        setDmg(12);
+    }
+}
